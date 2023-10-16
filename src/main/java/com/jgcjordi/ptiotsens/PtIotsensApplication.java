07f8a01208fffe3b51025666382661581eb10801
Jordi <32263782+jgcjordi@users.noreply.github.com>
2023-10-16 15:43:56 +0200

    added: spring initializer

---
+++ src/main/java/com/jgcjordi/ptiotsens/PtIotsensApplication.java
@@ -0,0 +1,13 @@
+package com.jgcjordi.ptiotsens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PtIotsensApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PtIotsensApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jgcjordi/ptiotsens/PtIotsensApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jgcjordi.ptiotsens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PtIotsensApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
